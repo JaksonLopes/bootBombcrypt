a8acb23ccff701c166cb38a08e69cd92dacf220c
jackson <jacksonlopes948@gmail.com>
2022-01-12 13:39:24 -0300

    versão final

--- botBombcrypt.py
+++ botBombcrypt.py
@@ -1,52 +1,87 @@
-
 import time
-import pyautogui
+from pynput.mouse import Button, Controller
 
-descansar = 'descansar.PNG'
-Trabalhar = 'trabalhar.PNG'
-fecharherois = 'fherois.PNG'
-mapa = 'mapa.PNG'
-fecharMapa = 'fmapa.PNG'
-entraHerois = 'herois.PNG'
-cheio = 'cheio.PNG'
+boot = Controller()
 
-boot = pyautogui
+
+print("===========================================================")
+print("----VAMOS INICIAR ,PREPARE-SE PARA RECUPERAR OS BOTÕES---- ")
+print("===========================================================")
+time.sleep(3)
+
+print("VC TEM 20 SEGUNDOS PRA NAVEGAR ATE OS BOTÕES")
+time.sleep(3)
+
+print("LEVE O MOUSE ATE A CASA DOS HEROIS ")
+time.sleep(20)
+entraHerois = boot.position
+print("PRONTO")
+
+print("LEVE O MOUSE ATE O BOTÃO DESCANSAR ")
+time.sleep(20)
+descansar = boot.position
+print("PRONTO")
+
+print("LEVE O MOUSE ATE O TRABALHAR")
+time.sleep(20)
+Trabalhar = boot.position
+print("PRONTO")
+
+print("LEVE O MOUSE ATE FECHAR CASA DOS HEROIS  ")
+time.sleep(20)
+fecharherois = boot.position
+print("PRONTO")
+
+print("LEVE O MOUSE ATE O MAPA ")
+time.sleep(20)
+mapa = boot.position
+print("PRONTO")
+
+print("LEVE O MOUSE PARA FECHA O MAPA ")
+time.sleep(20)
+fecharMapa = boot.position
+print("PRONTO")
+
+print("===========================================================")
+print("--------POSIÇÃO DOS BOTÕES RECUPERADOS COM SUCESSO ------- ")
+print("-----------DEIXA A TELA DO JOGO NA PAGINA INICIAL---------- ")
+print("===========================================================")
+
+time.sleep(20)
+print("===========================================================")
+print("--------PODE DEIXA PQ AGORA É COMIGO ,BOM DESCANSO ------- ")
+print("===========================================================")
 
 while True:
-    if boot.locateCenterOnScreen(cheio) or boot.locateCenterOnScreen(cheio):
-        print('entrei no descanço dos herois')
-        local = boot.locateCenterOnScreen(Trabalhar)
-        boot.click(local)
-        time.sleep(15)
 
-        local = boot.locateCenterOnScreen(fecharherois)
-        boot.click(local)
-        time.sleep(15)
+    boot.position =(entraHerois)
+    time.sleep(2)
+    boot.click(Button.left)
+    time.sleep(5)
 
-        for i in range(1,4):
-            print('entrei no mapa ')
-            local = boot.locateCenterOnScreen(mapa)
-            boot.click(local)
-            time.sleep(300)
+    boot.position =(descansar)
+    time.sleep(2)
+    boot.click(Button.left)
+    time.sleep(3600)
 
-            local = boot.locateCenterOnScreen(fecharMapa)
-            boot.click(local)
-            time.sleep(15)
-        
-        local = boot.locateCenterOnScreen(entraHerois)
-        boot.click(local)
-        time.sleep(15)
+    boot.position =(Trabalhar)
+    time.sleep(2)
+    boot.click(Button.left)
+    time.sleep(5)
 
-        local = boot.locateCenterOnScreen(descansar)
-        boot.click(local)
-        time.sleep(3600)
+    boot.position =(fecharherois)
+    time.sleep(2)
+    boot.click(Button.left)
+    time.sleep(5)
 
-        
+    for i in range(1,5):
 
-    elif boot.locateCenterOnScreen(mapa) or boot.locateCenterOnScreen(entraHerois):
-        print('?????????')
-        local = boot.locateCenterOnScreen(entraHerois)
-        boot.click(local)
+        boot.position =(mapa)
+        time.sleep(2)
+        boot.click(Button.left)
+        time.sleep(300)
+
+        boot.position =(fecharMapa)
+        time.sleep(2)
+        boot.click(Button.left)
         time.sleep(10)
-    else:
-        print('não achei nehuma imagem correspondente ')
