8c78f1e64376b7154368722da1a1ea9105dff787
jackson <jacksonlopes948@gmail.com>
2022-01-12 00:56:22 -0300

    codigo base funcional

--- botBombcrypt.py
+++ botBombcrypt.py
@@ -1,26 +1,52 @@
 
-from time import time
+import time
 import pyautogui
 
-Descansar = 'descansar.PNG'
+descansar = 'descansar.PNG'
 Trabalhar = 'trabalhar.PNG'
 fecharherois = 'fherois.PNG'
 mapa = 'mapa.PNG'
 fecharMapa = 'fmapa.PNG'
 entraHerois = 'herois.PNG'
+cheio = 'cheio.PNG'
 
+boot = pyautogui
 
+while True:
+    if boot.locateCenterOnScreen(cheio) or boot.locateCenterOnScreen(cheio):
+        print('entrei no descanço dos herois')
+        local = boot.locateCenterOnScreen(Trabalhar)
+        boot.click(local)
+        time.sleep(15)
 
-bot = pyautogui
+        local = boot.locateCenterOnScreen(fecharherois)
+        boot.click(local)
+        time.sleep(15)
 
-local = bot.locateCenterOnScreen(mapa)
+        for i in range(1,4):
+            print('entrei no mapa ')
+            local = boot.locateCenterOnScreen(mapa)
+            boot.click(local)
+            time.sleep(300)
 
+            local = boot.locateCenterOnScreen(fecharMapa)
+            boot.click(local)
+            time.sleep(15)
+        
+        local = boot.locateCenterOnScreen(entraHerois)
+        boot.click(local)
+        time.sleep(15)
 
-print(local)
+        local = boot.locateCenterOnScreen(descansar)
+        boot.click(local)
+        time.sleep(3600)
 
-for i in range(1,20000):
+        
 
-    time.sleep(2)
-    local = bot.locateCenterOnScreen(mapa)
-    bot.click(local)
-
+    elif boot.locateCenterOnScreen(mapa) or boot.locateCenterOnScreen(entraHerois):
+        print('?????????')
+        local = boot.locateCenterOnScreen(entraHerois)
+        boot.click(local)
+        time.sleep(10)
+    else:
+        print('não achei nehuma imagem correspondente ')
